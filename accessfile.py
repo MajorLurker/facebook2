98d04ac6566b5dddfac878e4ab60e59e35203431
Andrew Clinkaberry <myld69@hotmail.com>
2021-07-22 22:59:01 +1000

    First commit

---
+++ accessfile.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+
+def weeknumber():
+    f = open("c:\\pending\\weeknumber.txt", "r")
+    week = f.readline()
+    f.close()
+    return week
+
+
+def dailyupdate(tomorrow):
+    week_number = int(weeknumber())
+
+    programs = pd.read_excel('F:\\1 Work Folder\\Back to 3BBR\\3BBR Program Spreadsheet.ods')
+    print(programs)
+    programs.fillna("", inplace=True)
+    programs = programs.values.tolist()
+    show = []
+    for i in programs:
+        if i[0] == tomorrow:
+            if i[5] == week_number or i[5] == "":
+                i[1] = i[1].strftime("%I:%M%p")
+                show_time = i[1]
+                show_name = i[2]
+                show_presenter = i[3] + " " + i[4]
+                show.append(show_time + " - " + show_name + " presented by " + show_presenter)
+    return show
